68178f9f96e01d203205ce121d08c968c85c51b0
mtocron <kmtn.dx@gmail.com>
2019-01-27 22:20:18 +0900

    feat:4.1インタフェース

---
+++ src/Tester/Chapter4/ch4-interface-definition/ClassWithMethodSignatureClash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester.Chapter4.ch4_interface_definition
+{
+    public class ClassWithMethodSignatureClash : IInterfaceOne
+    {
+        public void MethodOne()
+        {
+
+        }
+
+        void IInterfaceOne.MethodOne()
+        {
+        }
+    }
+}
---
+++ src/Tester/Chapter4/ch4-interface-definition/ExplicitInterfaceClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester.Chapter4.ch4_interface_definition
+{
+    public class ExplicitInterfaceClient
+    {
+        public ExplicitInterfaceClient
+            (
+            ExplicitInterfaceImplementation implementationReference
+            , ISimpleInterface interfaceReference
+            )
+        {
+            // implementationReferenceではコンパイツエラーになる
+
+            var interfaceProperyValue = interfaceReference.ThisIntegerPropertyOnlyNeedGetter;
+            interfaceReference.ThisMethodRequiresImplementation();
+            interfaceReference.ThisStringPropertyNeedImplementingToo = "Hello";
+            interfaceReference.InterfacesCanContainEventsToo += EventHandler;
+        }
+
+        void EventHandler(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
